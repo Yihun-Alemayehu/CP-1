454c656675e18eb094a1e93956ffa8eb71fc3318
Yihun <yankure01@gmail.com>
2024-02-24 11:00:55 +0300

    fdb-02

---
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:cp_1/features/todo/domain/entities/todo.dart';
+import 'package:cp_1/features/todo/domain/repositories/todo_repository.dart';
+
+class TodoRepositoryImpl implements TodoRepository {
+
+  @override
+  Future<Todo> addTodo(Todo todo) async{
+    // TODO: implement addTodo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Todo> deleteTodo(Todo todo) async{
+    // TODO: implement deleteTodo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Todo> editTodo(Todo todo) async{
+    // TODO: implement editTodo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Todo>> getAllTodes() async{
+    // TODO: implement getAllTodes
+    throw UnimplementedError();
+  }
+
+}
--- lib/features/todo/domain/repositories/todo_repository.dart
+++ lib/features/todo/domain/repositories/todo_repository.dart
@@ -1,3 +1,15 @@
+import 'package:cp_1/features/todo/domain/entities/todo.dart';
+
 abstract class TodoRepository {
-  
+  // add todo:
+  Future<Todo> addTodo(Todo todo);
+
+  // edit todo:
+  Future<Todo> editTodo(Todo todo);
+
+  // delete todo:
+  Future<Todo> deleteTodo(Todo todo);
+
+  // get all todos:
+  Future<List<Todo>> getAllTodes();
 }
